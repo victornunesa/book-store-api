2083703df089826a81a3c47181e1afcb1d90f1f2
Victor Nunes <victor.nunes@opovodigital.com>
2024-04-07 12:19:21 -0300

    feat: add exception handling

--- application/bootstrap/app.php
+++ application/bootstrap/app.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Responses\ApiErrorResponse;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Http\Response;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,5 +21,25 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (Throwable $e, Request $request) {
+            if ($request->is('api/*')) {
+                $code = $e->status ?? null;
+
+                if (empty($code)) {
+                    if ($e instanceof NotFoundHttpException) {
+                        $code = Response::HTTP_NOT_FOUND;
+                    } elseif ($e instanceof AuthenticationException) {
+                        $code = Response::HTTP_UNAUTHORIZED;
+                    } elseif ($e instanceof ValidationException) {
+                        $code = Response::HTTP_UNPROCESSABLE_ENTITY;
+                    } else {
+                        $code = Response::HTTP_INTERNAL_SERVER_ERROR;
+                    }
+                }
+
+                return new ApiErrorResponse($e, $e->getMessage(), $code);
+            }
+
+            return $request->expectsJson();
+        });
     })->create();
